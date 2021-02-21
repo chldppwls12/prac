2d9c32bd6722a5395c8e949b9ca6b463637eb781
chldppwls12 <chldppwls12@naver.com>
2021-02-21 19:37:57 +0900

    header, footer, login 창 추가

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,45 @@
-import React, {Component} from 'react';
-import {Router, Route, Switch} from "react-router";
+import React, { Component } from 'react'
+import { Router, Route, Switch } from 'react-router'
 
 import Api_test from './Api_test'
+import css from '../css/new.css'
+import HeaderAdmin from './Header/Header admin'
+import Footer from './Footer/Footer'
+import LoginForm from './LoginForm'
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-    }
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
   }
-  componentDidMount(){}
-  render(){
+  componentDidMount() {}
+  render() {
     return (
       ///Api_test 경로로 유입된 호출은 Api_test.js 파일로 이동
-      <div className = "App">
+      <div className="App">
+        <HeaderAdmin />
         <Switch>
-          <Route exact path = '/' component={Api_test} />
-          <Route path = '/Api_test' component = {Api_test}/>
+          <Route exact path="/" component={LoginForm} />
+          <Route path="/Api_test" component={Api_test} />
         </Switch>
+        <Footer
+          footer_address={this.props.footer_address}
+          footer_tel={this.props.footer_tel}
+          footer_email={this.props.footer_email}
+          footer_mobile={this.props.footer_mobile}
+        />
       </div>
     )
   }
 }
 
-export default App;
+App.defaultProps = {
+  footer_address: 'footer_address',
+  footer_tel: 'footer_tel',
+  footer_email: 'footer_email',
+  footer_mobile: 'footer_mobile',
+}
+
+export default App
